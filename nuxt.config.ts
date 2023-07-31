c890b52dce5b44d1829b9727a0deaccec5876dd7
zhangliang <2424123787@qq.com>
2023-07-31 14:27:02 +0800

    绘画

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
     app: {
 
         head: {
-           title: '1Chat问答绘图',
+           title: '1Chat问答绘画',
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
